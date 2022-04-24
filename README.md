ff37f9d94d707b3c6b8cf809d15c59585027e458
Lagver <79370817+Lagver@users.noreply.github.com>
2022-04-24 14:32:51 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# FPShooter
+
+Developed with Unreal Engine 4
---
+++ Source/FPShooter/FPShooterGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FPShooterGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FPSHOOTER_API AFPShooterGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
